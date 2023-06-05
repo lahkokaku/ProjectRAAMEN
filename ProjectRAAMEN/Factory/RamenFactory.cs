02e0d002e28deedc0a0c88de60739dd7b5184546
lahkokaku <jose.satrya@gmail.com>
2023-06-05 18:06:02 +0700

    fix: fixed Ramen table column name

---
+++ ProjectRAAMEN/Factory/RamenFactory.cs
@@ -0,0 +1,22 @@
+﻿using ProjectRAAMEN.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectRAAMEN.Factory
+{
+    public class RamenFactory
+    {
+        public static Raman CreateRamen(string RamenName, int MeatId, string Broth, int Price)
+        {
+            Raman NewRamen = new Raman();
+            NewRamen.Name = RamenName;
+            NewRamen.MeatId = MeatId;
+            NewRamen.Broth = Broth;
+            NewRamen.Price = Price.ToString();
+
+            return NewRamen;
+        }
+    }
+}
